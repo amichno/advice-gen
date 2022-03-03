5cf8f8c72b29025954eeee6b68f74068dd73c233
amichno <95045327+amichno@users.noreply.github.com>
2022-03-03 14:45:19 +0100

    push -A

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component{
+
+  constructor(){
+    super()
+   
+  }
+
+  changeAdvice(){
+    const paragraf = document.getElementsByClassName("adv");
+    console.log(paragraf[0].textContent);
+    //let old_adv = this.getAdvice("adv");
+    //this.connectApi();
+
+    const url_api = "https://api.adviceslip.com/advice";
+    fetch(url_api).then(response=>response.json()).then(jsonRes => console.log(jsonRes));
+
+    //const url_api = "https://api.adviceslip.com/advice";
+   /* let data = null;
+    const response = axios.get(url_api).then(resp => data=resp.data);
+    const jsonresp = response.json;
+    console.log(); */     
+  }
+
+
+
   render(){
   return (
     <div className="container">
@@ -14,7 +39,7 @@ class App extends React.Component{
                 
             </div>
             <div className="dice">  
-                  <button className ="button-dice"  ></button>        
+                  <button className ="button-dice" onClick={this.changeAdvice} ></button>        
             </div>
         </div>
   );
