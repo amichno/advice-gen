058d3d5949db5df4ccfe27cf8e6343fc28b8b648
amichno <95045327+amichno@users.noreply.github.com>
2022-03-04 09:23:05 +0100

    push -A

--- src/App.js
+++ src/App.js
@@ -13,36 +13,33 @@ class App extends React.Component{
   changeAdvice(){
     const paragraf = document.getElementsByClassName("adv");
     console.log(paragraf[0].textContent);
-    //let old_adv = this.getAdvice("adv");
-    //this.connectApi();
 
     const url_api = "https://api.adviceslip.com/advice";
-    fetch(url_api).then(response=>response.json()).then(jsonRes => console.log(jsonRes));
-
-    //const url_api = "https://api.adviceslip.com/advice";
-   /* let data = null;
-    const response = axios.get(url_api).then(resp => data=resp.data);
-    const jsonresp = response.json;
-    console.log(); */     
+    fetch(url_api).then(
+                        response=>response.json()).then(
+                          jsonRes => {
+                            console.log(jsonRes.data.id)
+                          }
+                          );
+    
   }
 
 
-
   render(){
-  return (
-    <div className="container">
-            <div className="Advice">
-                <div className="api">
-                    <p className="adv">Advice</p>
-                    <p>Text</p>
-                </div>
-                
-            </div>
-            <div className="dice">  
-                  <button className ="button-dice" onClick={this.changeAdvice} ></button>        
-            </div>
-        </div>
-  );
+    return (
+      <div className="container">
+              <div className="Advice">
+                  <div className="api">
+                      <p className="adv">Advice</p>
+                      <p>Text</p>
+                  </div>
+                  
+              </div>
+              <div className="dice">  
+                    <button className ="button-dice" onClick={this.changeAdvice} ></button>        
+              </div>
+          </div>
+    );
   }
 }
 
